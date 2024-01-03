79a5b370ecb570652a3d47502e16136229e5cfe6
Mayank jaiswal <mayank.jazz21@gmail.com>
2024-01-03 14:48:23 +0530

    first commit

---
+++ demo.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+   int a,b,c;
+   cin>>a>>b>>c;
+   if(a>b){
+    if(a>c){
+        cout<< "A is greatest";
+    }
+    else{
+        cout<<"C is greatest";
+
+    }
+   }
+   else{
+    if(b>c){
+        cout<<" B is grtst";
+    }
+    else{
+        cout<< "c is grtst";
+    }
+   }
+    return 0;
+}
